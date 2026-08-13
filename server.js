c1bdfd99ca04e093518e4248df80750545135c10
kosibsr882-rgb <kosibsr882@gmail.com>
2026-08-13 08:56:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -69,10 +69,10 @@ app.post('/api/send-email', requireLogin, async (req, res) => {
         to: recipient,
         subject,
         html: `
-          <div style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; font-weight: bold; line-height: 1.5;">
+          <div style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; line-height: 1.5;">
             ${messageBody}
           </div>
-          <div style="font-family: Arial, Helvetica, sans-serif; font-size: 10px; font-weight: bold; margin-top: 20px; color: #555;">
+          <div style="font-family: Arial, Helvetica, sans-serif; font-size: 11px; margin-top: 20px; color: #555;">
             Thanks & Regards,<br>
             ${senderName || gmailId}
           </div>
