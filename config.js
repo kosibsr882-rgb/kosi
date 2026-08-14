e8b19a96f53fb914abc6fc7cd27d772ffb9b1783
kosibsr882-rgb <kosibsr882@gmail.com>
2026-08-14 23:20:25 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-require('dotenv').config();
-
-module.exports = {
-  adminUser: process.env.ADMIN_USER || 'admin',
-  adminPass: process.env.ADMIN_PASS || 'admin123',
-  sessionSecret: process.env.SESSION_SECRET || 'fast-mailer-secret',
-  port: process.env.PORT || 3000
-};
